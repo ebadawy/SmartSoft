cf12028375552ed39fd9c3ef2ca9ec893d4fffee
MohamedEmad93 <mohame.emad.9307@gmail.com>
2014-09-25 21:54:34 +0300

    database migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140925185118) do
+
+  create_table "offers", force: true do |t|
+    t.string   "book_name"
+    t.integer  "old_price"
+    t.integer  "new_price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
